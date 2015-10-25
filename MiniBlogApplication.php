f6381882cfe396ec199cdbabf03567f9c11d484c
ksugawara61 <katsuo.com61@gmail.com>
2015-10-25 15:07:23 +0100

    MiniBlogApplication.phpの追加

---
+++ MiniBlogApplication.php
@@ -0,0 +1,30 @@
+<?php
+
+class MiniBlogApplication extends Application
+{
+	// メンバ変数
+	protected $login_action = array('account', 'signin');
+
+	// ルートディレクトリへのパスを返すメソッド
+	public function getRootDir()
+	{
+		return dirname(__FILE__);
+	}
+
+	// ルーティング定義配列を返すメソッド
+	protected function registerRoutes()
+	{
+		return array(
+		);
+	}
+
+	// アプリケーションの設定を行うメソッド
+	protected function configure()
+	{
+		$this->db_manager->connect('master', array(
+			'dsn' => 'mysql:dbname=mini_blog;host=lcalhost',
+			'user' => 'root',
+			'password' => '',
+		));	
+	}
+}
