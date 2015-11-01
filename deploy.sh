7c292cc6eaca6b298446b1077663c1482d8f0ebf
ksugawara61 <katsuo.com61@gmail.com>
2015-11-01 10:28:48 +0100

    デプロイ処理の修正

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,13 @@
 dir=/var/www/html/mini-blog.localhost
 
 if [ ! -e $dir ]; then
-	mkdir /var/www/html/mini-blog.localhost
+	sudo mkdir /var/www/html/mini-blog.localhost
 fi
 
 sudo cp MiniBlogApplication.php /var/www/html/mini-blog.localhost/
 sudo cp bootstrap.php /var/www/html/mini-blog.localhost/
+sudo cp -r controllers /var/www/html/mini-blog.localhost/
 sudo cp -r core /var/www/html/mini-blog.localhost/
 sudo cp -r views /var/www/html/mini-blog.localhost/
+sudo cp -r models /var/www/html/mini-blog.localhost/
 sudo cp -r web /var/www/html/mini-blog.localhost/
