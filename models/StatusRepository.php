0a1397cc7b4783a3abb5e748a863c59c46fffeb7
ksugawara61 <katsuo.com61@gmail.com>
2015-11-15 08:07:53 +0100

    StatusRepository.phpの修正：SQL文の誤りを修正

--- models/StatusRepository.php
+++ models/StatusRepository.php
@@ -17,7 +17,7 @@ class StatusRepository extends DbRepository
 
 	public function fetchAllPersonalArchivesByUserId($user_id)
 	{
-		$sql = "SELECT a.*, u.user_name FROME status a LEFT JOIN user u ON a.user_id = u.id WHERE u.id = :user_id ORDER BY a.ceated_at DESC";
+		$sql = "SELECT a.*, u.user_name FROM status a LEFT JOIN user u ON a.user_id = u.id WHERE u.id = :user_id ORDER BY a.created_at DESC";
 
 		return $this->fetchAll($sql, array(':user_id' => $user_id));
 	}
