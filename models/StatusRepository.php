e7e6e855371a6562e3d7c7da43f3078415846e05
ksugawara61 <katsuo.com61@gmail.com>
2015-11-23 14:10:33 +0100

    userアクションの追加

--- models/StatusRepository.php
+++ models/StatusRepository.php
@@ -40,4 +40,20 @@ class StatusRepository extends DbRepository
 			':user_name' => $user_name,
 		));
 	}
+
+	// userアクションメソッド
+	public function userAction($params)
+	{
+		$user = $this->db_manager->get('User')->fetchByUserName($params['user_name']);
+		if (!$user) {
+			$this->forward404();
+		}
+
+		$statuses = $this->db_manager->get('Status')->fetchAllByUserId($user['id']);
+
+		return $this->render(array(
+			'user' => $user,
+			'statuses' => $statuses,
+		));
+	}
 }
