e984ae88d4935539353991f78745e878d9abb6e6
ksugawara61 <katsuo.com61@gmail.com>
2015-12-23 10:22:12 +0100

    ユーザページにコメントを表示

--- views/status/user.php
+++ views/status/user.php
@@ -14,3 +14,9 @@
 		</form>
 	<?php endif; ?>
 <?php endif; ?>
+
+<div id="statuses">
+	<?php foreach ($statuses as $status): ?>
+		<?php echo $this->render('status/status', array('status' => $status)); ?>
+	<?php endforeach; ?>
+</div>
